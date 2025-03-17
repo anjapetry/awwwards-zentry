378a1d362e20dc7ae795711419b8912b3b14210f
Anja Petry <anjapetry@protonmail.com>
2025-03-17 21:48:26 +0100

    styles: add fonts to tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,15 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        zentry: ['zentry', 'sans-serif'],
+      general: ['general', 'sans-serif'],
+        'circular-web': ['circular-web', 'sans-serif'],
+        'robert-medium': ['general', 'sans-serif'],
+        'robert-regular': ['general', 'sans-serif'],
+      }
+    },
   },
   plugins: [],
 }
