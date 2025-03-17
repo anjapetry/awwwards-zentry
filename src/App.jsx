18f06dbf139c257e6bb1e04b0ee6a8f1c35c1b01
Anja Petry <anjapetry@protonmail.com>
2025-03-17 21:59:57 +0100

    styles: add body styles to index.css

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const App = () => {
     return (
         <main>
-        <h1 className="text-3xl font-bold">Welcome to Zentry!
+        <h1 className="text-5xl font-bold">Welcome to Zentry!
         </h1>
         </main>
     )
