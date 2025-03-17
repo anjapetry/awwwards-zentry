5597b2ec2ebe0310e99c3319fd631c3cedff53a7
Anja Petry <anjapetry@protonmail.com>
2025-03-17 22:06:57 +0100

    styles: extend color theme in tailwind.cofig.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,11 +8,27 @@ export default {
     extend: {
       fontFamily: {
         zentry: ['zentry', 'sans-serif'],
-      general: ['general', 'sans-serif'],
+        general: ['general', 'sans-serif'],
         'circular-web': ['circular-web', 'sans-serif'],
         'robert-medium': ['general', 'sans-serif'],
         'robert-regular': ['general', 'sans-serif'],
-      }
+      },
+      colors: {
+        blue: {
+          50: "#DFDFF0",
+          75: "#dfdff2",
+          100: "#F0F2FA",
+          200: "#010101",
+          300: "#4FB7DD",
+        },
+        violet: {
+          300: "#5724ff",
+        },
+        yellow: {
+          100: "#8e983f",
+          300: "#edff66",
+        },
+      },
     },
   },
   plugins: [],
