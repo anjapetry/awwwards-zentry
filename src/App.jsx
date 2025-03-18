c1e3ef2d6cd80194771f27fe3742620f0550bbb9
Anja Petry <anjapetry@protonmail.com>
2025-03-18 20:33:51 +0100

    feat: add About.jsx to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
-import Hero from './components/Hero.jsx'
+import React from "react";
+import Hero from "./components/Hero.jsx";
+import About from "./components/About.jsx";
 
 const App = () => {
-    return (
-        <main>
-       <Hero />
-        </main>
-    )
-}
-export default App
+  return (
+    <main>
+      <Hero />
+      <About />
+    </main>
+  );
+};
+export default App;
