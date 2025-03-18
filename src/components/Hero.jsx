44efb2c6bccc61f9bdd43a95d82c7bb04e6873c7
Anja Petry <anjapetry@protonmail.com>
2025-03-18 17:01:43 +0100

    feat: implenment videos in Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,15 +1,94 @@
-import React from "react";
+import { useRef, useState } from "react";
 
 const Hero = () => {
+  const [currentIndex, setCurrentIndex] = useState(1);
+  const [hasClicked, setHasClicked] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+  const [loadedVideos, setLoadedVideos] = useState(0);
+
+  const totalVideos = 4;
+  const nextVideoRef = useRef(null);
+
+  const handleVideoLoad = () => {
+    setLoadedVideos((prev) => prev + 1);
+
+    if (loadedVideos === totalVideos) {
+      setIsLoading(false);
+    }
+  };
+
+  // 0 % 4 = 0 + 1 = 1
+  // 1 % 4 = 1 + 1 = 2
+  // 2 % 4 = 2 + 1 = 3
+  // 3 % 4 = 3 + 1 = 4
+  // 4 % 4 = 0 + 1 = 1
+  const upcomingVideoIndex = (currentIndex % totalVideos) + 1;
+
+  const handleMiniVideoClick = () => {
+    setHasClicked(true);
+
+    setCurrentIndex(upcomingVideoIndex);
+  };
+
+  const getVideoSrc = (index) => `videos/hero-${index}.mp4`;
+
   return (
     <div className="relative h-dvh w-full overflow-x-hidden">
-      Hero
       <div
         id="video-frame"
         className="relative z-10 h-dvh w-screen overflow-hidden rounded-lg bg-blue-75"
       >
         <div>
-          <div className="mask-clip-path"></div>
+          <div className="mask-clip-path absolute-center absolute z-50 size-64 cursor-pointer overflow-hidden rounded-lg">
+            <div
+              onClick={handleMiniVideoClick}
+              className="ease--in origin-center scale-50 opacity-0 transition-all duration-500 hover:scale-100 hover:opacity-100"
+            >
+              <video
+                ref={nextVideoRef}
+                src={getVideoSrc(upcomingVideoIndex)}
+                loop
+                muted
+                id="current-video"
+                className="size-64 origin-center scale-150 object-cover object-center"
+                onLoadedData={handleVideoLoad}
+              />
+            </div>
+          </div>
+
+          <video
+            ref={nextVideoRef}
+            src={getVideoSrc(currentIndex)}
+            loop
+            muted
+            id="next-video"
+            className="absolute-center invisible absolute z-20 size-64 object-cover object-center"
+            onLoadedData={handleVideoLoad}
+          />
+          <video
+            src={getVideoSrc(
+              currentIndex === totalVideos - 1 ? 1 : currentIndex + 1,
+            )}
+            autoPlay
+            loop
+            muted
+            className="absolute left-0 top-0 size-full object-cover object-center"
+            onLoadedData={handleVideoLoad}
+          />
+        </div>
+        <h1 className="special-font hero-heading absolute bottom-5 right-5 z-40 text-blue-75">
+          G<b>a</b>ming
+        </h1>
+
+        <div className="absolute left-0 top-0 z-40 size-full">
+          <section className="mt-24 px-5 sm:px-10">
+            <h1 className="special-font hero-heading text-blue-100">
+              redefi<b>n</b>e
+            </h1>
+            <p className="mb-5 max-w-64 font-robert-regular text-blue-100">
+              Enter the Metagame Layer <br /> Unleash the Power of Play
+            </p>
+          </section>
         </div>
       </div>
     </div>
