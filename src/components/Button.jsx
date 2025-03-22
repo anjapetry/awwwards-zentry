cde2241bd78cf2ead7274dcba4d060314ce2f333
Anja Petry <anjapetry@protonmail.com>
2025-03-22 22:18:40 +0100

    styles: modify Button.jsx

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,14 +1,25 @@
-import React from 'react'
+import React from "react";
+import clsx from "clsx";
 
-const Button = ({title, id, leftIcon, rightIcon, containerClass }) => {
-    return (
-       <button type="button" id={id} className={`group relative z-10 w-fit cursor-pointer overflow-hidden rounded-full bg-violet-50 font-semibold px-7 py-3 text-black 
-        hover:bg-teal-100 focus:outline-none focus:ring-4 focus:ring-teal-950 text-center me-2 mb-2" ${containerClass}`}>
-              {leftIcon}
-              <span className="relative inline-flex overflow-hidden font-genral text-sm uppercase" aria-label="Watch Trailer">
-                  {title}</span>
-              {rightIcon}
-       </button>
-    )
-}
-export default Button
+const Button = ({ title, id, leftIcon, rightIcon, containerClass }) => {
+  return (
+    <button
+      type="button"
+      id={id}
+      className={clsx(
+        "group relative z-10 mb-2 me-2 w-fit cursor-pointer overflow-hidden rounded-full bg-violet-50 px-7 py-3 text-center font-semibold text-black hover:bg-teal-100 focus:outline-none focus:ring-4 focus:ring-teal-950",
+        containerClass,
+      )}
+    >
+      {leftIcon}
+      <span
+        className="relative inline-flex overflow-hidden font-general text-sm uppercase"
+        aria-label="Watch Trailer"
+      >
+        {title}
+      </span>
+      {rightIcon}
+    </button>
+  );
+};
+export default Button;
