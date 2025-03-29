eb01d32a3865c6b9fea4f59e7a5480792334e6ae
Anja Petry <anjapetry@protonmail.com>
2025-03-29 16:37:27 +0100

    feat: add Features.jsx to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Navbar from "./components/Navbar.jsx";
 import Hero from "./components/Hero.jsx";
 import About from "./components/About.jsx";
+import Features from "./components/Features.jsx";
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <Navbar />
       <Hero />
       <About />
+      <Features />
     </main>
   );
 };
