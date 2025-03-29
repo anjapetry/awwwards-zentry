a83b20a6fb13680523ce13af59828541f462a281
Anja Petry <anjapetry@protonmail.com>
2025-03-29 14:49:48 +0100

    feat: create Features.jsx

---
+++ src/components/Features.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const Features = () => {
+  return (
+    <section className="bg-black pb-52">
+      <div className="container mx-auto px-3 md:px-10">
+        <div className="px-5 py-32">
+          <p className="font-circular-web text-lg text-blue-50">
+            Into the MetaGame Layer{" "}
+          </p>
+        </div>
+        <p className="font-circular-web text-lg text-blue-50">
+          Immerse yourself in a rich and ever-expanding universe, where a
+          vibrant array of products converge into an interconnected overlay on
+          your world.
+        </p>
+        <p className="max-w-md font-circular-web text-lg text-blue-50 opacity-70">
+          Discover the limitless possibilities and experiences that await you.
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default Features;
