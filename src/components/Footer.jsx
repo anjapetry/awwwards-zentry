374d40ce0a582dd2991825f03369c5badff8e537
Anja Petry <anjapetry@protonmail.com>
2025-04-04 16:23:23 +0200

    feat: add social links to Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,48 @@
+import { FaDiscord, FaTwitter, FaYoutube, FaMedium } from "react-icons/fa";
+
+const socialLinks = [
+  { href: "https://discord.com", icon: <FaDiscord /> },
+  { href: "https://twitter.com", icon: <FaTwitter /> },
+  { href: "https://youtube.com", icon: <FaYoutube /> },
+  { href: "https://medium.com", icon: <FaMedium /> },
+];
+
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="w-screen bg-[#5542ff] py-4 text-violet-50">
+      <div className="container mx-auto flex flex-col items-center justify-between gap-4 px-4 md:flex-row">
+        <p className="text-center text-base font-light md:text-left">
+          ©Nova 2024. All rights reserved
+        </p>
+
+        <div className="flex justify-center gap-4 md:justify-start">
+          {socialLinks.map((link, index) => (
+            <a
+              key={index}
+              href={link.href}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-violet-50 transition-colors duration-500 ease-in-out hover:scale-125 hover:text-teal-100"
+              aria-label={`Link to ${link.href}`}
+              title={`Link to ${link.href}`}
+              onClick={() => {
+                window.open(link.href, "_blank");
+              }}
+            >
+              {link.icon}
+            </a>
+          ))}
+        </div>
+
+        <a
+          href="#privacy-policy"
+          className="text-bases text-center font-light hover:underline md:text-right"
+        >
+          Privacy Policy
+        </a>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
