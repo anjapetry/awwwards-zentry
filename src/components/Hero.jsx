6b6c08810baf2915e28fd8cdc56ba456756e5d17
Anja Petry <52009580+anjapetry@users.noreply.github.com>
2026-05-22 14:36:47 +0200

    fix(hero): modify mini video player

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -146,7 +146,7 @@ const Hero = () => {
         className="relative z-10 h-dvh w-screen overflow-hidden rounded-lg bg-blue-75"
       >
         <div>
-          <div className="mask-clip-path absolute-center absolute z-50 size-64 cursor-pointer overflow-hidden rounded-lg">
+          <div className="mask-clip-path absolute-center absolute z-50 mt-10 size-64 cursor-pointer overflow-hidden rounded-lg">
             <button
               type="button"
               onClick={handleMiniVideoClick}
